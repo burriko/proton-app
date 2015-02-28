b4c70fa3d2c77091ea6d52f98422f9150a8a71f8
Graeme Tait <burriko@gmail.com>
2015-02-28 11:54:18 +0000

    Formatting

--- system/ServiceProvider/PlatesProvider.php
+++ system/ServiceProvider/PlatesProvider.php
@@ -6,11 +6,11 @@ use League\Container\ServiceProvider;
 
 class PlatesProvider extends ServiceProvider
 {
-	protected $provides = [\League\Plates\Engine::class];
+    protected $provides = [\League\Plates\Engine::class];
 
-	public function register()
-	{
-		$this->getContainer()->singleton(\League\Plates\Engine::class)
-		                     ->withArgument(__DIR__.'/../../resources/templates/');
-	}
+    public function register()
+    {
+        $this->getContainer()->singleton(\League\Plates\Engine::class)
+                             ->withArgument(__DIR__.'/../../resources/templates/');
+    }
 }
